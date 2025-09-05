10b9de763d48b8508d3a74c2d12ebf2f41ea5837
it24100038 <it24100038@my.sliit.lk>
2025-09-05 12:36:20 +0530

    lab 6

---
+++ Lab Sheet 06/IT24100038.R
@@ -0,0 +1,5 @@
+setwd("C:\\Users\\thinu\\OneDrive - Sri Lanka Institute of Information Technology\\SLIIT\\2nd year\\1st semester\\PS\\IT24101204-Lab6")
+
+1- pbinom(46, 50, 0.85, lower.tail = TRUE)
+
+dpois(15, 12)
